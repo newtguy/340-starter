8ad84212b2fb8888d3034e26d60d962f076358fa
newt <nate.e.shelley@gmail.com>
2026-04-04 14:03:11 -0600

    Account Registration & Validation (clientside & server)

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -1,12 +1,12 @@
-// Needed Resources 
+// Needed Resources
 const express = require("express")
-const router = new express.Router() 
+const router = new express.Router()
 const invController = require("../controllers/invController")
 
 // Route to build inventory by classification view
-router.get("/type/:classificationId", invController.buildByClassificationId);
+router.get("/type/:classificationId", invController.buildByClassificationId)
 
 // Route to build vehicle detail view
-router.get("/detail/:inv_id", invController.buildVehicleDetailView);
+router.get("/detail/:inv_id", invController.buildVehicleDetailView)
 
-module.exports = router;
+module.exports = router
--- server.js
+++ server.js
@@ -12,8 +12,38 @@ const app = express()
 const static = require("./routes/static")
 const baseController = require("./controllers/baseController")
 const inventoryRoute = require("./routes/inventoryRoute")
-const errorRoute = require("./routes/errorRoute");
+const errorRoute = require("./routes/errorRoute")
+const accountRoute = require("./routes/accountRoute")
 const utilities = require("./utilities")
+const session = require("express-session")
+const pool = require("./database/")
+const bodyParser = require("body-parser")
+
+/* ***********************
+ * Middleware
+ * ************************/
+app.use(
+  session({
+    store: new (require("connect-pg-simple")(session))({
+      createTableIfMissing: true,
+      pool,
+    }),
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true,
+    name: "sessionId",
+  }),
+)
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+// Express Messages Middleware
+app.use(require("connect-flash")())
+app.use(function (req, res, next) {
+  res.locals.messages = require("express-messages")(req, res)
+  next()
+})
 
 /* ***********************
  * View Engine and Templates
@@ -30,6 +60,8 @@ app.use(static)
 app.get("/", utilities.handleErrors(baseController.buildHome))
 //Inventory routes
 app.use("/inv", inventoryRoute)
+//Account route
+app.use("/account", accountRoute)
 //Intentional error route
 app.use("/err", errorRoute);
 // File Not Found Route - must be last route in list
